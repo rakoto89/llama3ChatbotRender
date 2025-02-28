902be9425ba1c5f98311953dd40a904ff4254f87
rakoto89 <121390005+rakoto89@users.noreply.github.com>
2025-02-27 19:33:13 -0500

    Update script.js

--- static/script.js
+++ static/script.js
@@ -16,8 +16,7 @@ document.addEventListener("DOMContentLoaded", function () {
         // Add bot image for bot messages
         if (sender === "bot") {
             const botImg = document.createElement("img");
-            botImg.src = "/static/robot.png"; // Path to bot image
-            botImg.width = 24;
+            botImg.src = "{{ url_for('static', filename='robot.png') }}";
             botImg.height = 24;
             botImg.alt = "Bot";
             botImg.style.marginRight = "8px"; // Adds spacing
