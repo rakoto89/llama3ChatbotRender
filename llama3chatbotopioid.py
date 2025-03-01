6eb35c93fbf2b88725c5d0219cf17435c71afff2
rakoto89 <121390005+rakoto89@users.noreply.github.com>
2025-03-01 08:44:29 -0500

    Update llama3chatbotopioid.py

--- llama3chatbotopioid.py
+++ llama3chatbotopioid.py
@@ -39,7 +39,7 @@ pdf_text = extract_text_from_pdf(pdf_paths)
 # List of relevant opioid-related keywords
 relevant_topics = [
     "opioids", "addiction", "overdose", "withdrawal", "fentanyl", "heroin", 
-    "painkillers", "narcotics", "opioid crisis", "naloxone", "rehab", "opiates", "opium", "substance abuse", "drugs", "tolerance", "opiates", "help", "assistance", "support" "opium", "opiate", "drug" "email", "campus", "phone number", "BSU", "Bowie State University", "opioid", "use", "disorder", "opioid use", "opioid disorder", "opioid usage", "usage", "taking opioids", "taking", "recreational use", "opioid dependence", "opioid self-medication", "self medication"
+    "painkillers", "narcotics", "opioid crisis", "naloxone", "rehab", "opiates", "opium", "substance abuse", "drugs", "tolerance", "opiates", "help", "assistance", "support for opioid addiction", "support for opioid" "opium", "opiate", "drug" "email", "campus", "phone number", "BSU", "Bowie State University", "opioid", "use", "disorder", "opioid use", "opioid disorder", "opioid usage", "usage", "taking opioids", "taking", "recreational use", "opioid dependence", "opioid self-medication", "self medication"
 ]
 
 def is_question_relevant(question):
