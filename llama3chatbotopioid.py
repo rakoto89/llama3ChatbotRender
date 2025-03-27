bf50fa3343ac63afdc47085d6697de32c9c6bf77
rakoto89 <121390005+rakoto89@users.noreply.github.com>
2025-03-27 08:18:35 -0400

    Update llama3chatbotopioid.py

--- llama3chatbotopioid.py
+++ llama3chatbotopioid.py
@@ -49,7 +49,7 @@ relevant_topics = [
     "students", "teens", "adults", "substance abuse", "drugs", "tolerance", "help", "assistance",
     "support", "support for opioid addiction", "drug use", "email", "campus", "phone number",
     "BSU", "Bowie State University", "opioid use disorder", "opioid self-medication", "self medication",
-    "number", "percentage", "symptoms", "signs"
+    "number", "percentage", "symptoms", "signs", "opioid abuse"
 ]
 
 # List of websites to scrape content from
