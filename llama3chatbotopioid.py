347d329087ad4ada6d9a8bee552e8f75fa008cff
rakoto89 <121390005+rakoto89@users.noreply.github.com>
2025-03-28 21:18:53 -0400

    Update llama3chatbotopioid.py

--- llama3chatbotopioid.py
+++ llama3chatbotopioid.py
@@ -45,7 +45,7 @@ relevant_topics = [
     "support", "support for opioid addiction", "drug use", "email", "campus", "phone number",
     "BSU", "Bowie State University", "opioid use disorder", "opioid self-medication", "self medication",
     "number", "percentage", "symptoms", "signs", "opioid abuse", "opioid misuse", "physical dependence", "prescription"
-    "medication-assistanted treatment",   "medication assistanted treatment" "MAT", "opioid epidemic"
+    "medication-assistanted treatment",   "medication assistanted treatment", "MAT", "opioid epidemic", "teen", "adult", "death"
 ]
 
 def load_urls_from_file(file_path):
