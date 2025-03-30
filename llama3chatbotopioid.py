ca89503b588a654df8a30404674f42c701af60d0
rakoto89 <121390005+rakoto89@users.noreply.github.com>
2025-03-30 14:18:38 -0400

    Update llama3chatbotopioid.py

--- llama3chatbotopioid.py
+++ llama3chatbotopioid.py
@@ -53,7 +53,7 @@ relevant_topics = [
     "number", "percentage", "symptoms", "signs", "opioid abuse", "opioid misuse", "physical dependence", "prescription",
     "medication-assisted treatment", "MAT", "opioid epidemic", "teen", "dangers", "genetic", "environmental factors",
     "pain management", "socioeconomic factors", "consequences", "adult", "death",
-    "semi-synthetic opioids", "neonatal abstinence syndrome", "NAS", brands
+    "semi-synthetic opioids", "neonatal abstinence syndrome", "NAS", "brands"
 ]
 
 # Contextual follow-up keywords (for related follow-up questions)
