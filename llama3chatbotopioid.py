edaa2ac3ba4b7b8101ac85415720666d02e6b4f7
rakoto89 <121390005+rakoto89@users.noreply.github.com>
2025-04-05 23:17:18 -0400

    Update llama3chatbotopioid.py

--- llama3chatbotopioid.py
+++ llama3chatbotopioid.py
@@ -247,3 +247,9 @@ def voice_input():
 
 if __name__ == "__main__":
     app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
