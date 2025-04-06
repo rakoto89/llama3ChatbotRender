012469a708783e2ec914c1e5c0fa3de72304e766
rakoto89 <121390005+rakoto89@users.noreply.github.com>
2025-04-05 23:20:40 -0400

    Update llama3chatbotopioid.py

--- llama3chatbotopioid.py
+++ llama3chatbotopioid.py
@@ -249,7 +249,3 @@ if __name__ == "__main__":
     app.run(debug=True)
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
