c127e9286d4c9a21aba339cc0927778d64f375e7
rakoto89 <121390005+rakoto89@users.noreply.github.com>
2025-04-09 18:57:52 -0400

    Update llama3chatbotopioid.py

--- llama3chatbotopioid.py
+++ llama3chatbotopioid.py
@@ -202,5 +202,5 @@ def feedback():
 
 # === Run App ===
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 1000))
     app.run(host="0.0.0.0", port=port)
